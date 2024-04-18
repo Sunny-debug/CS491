27f8b8bfd4bf8c51e861562e15230a1acac052a6
Sunny-debug <60430278+Sunny-debug@users.noreply.github.com>
2024-04-18 09:05:38 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+<form name="myForm" action="/action_page.php" onsubmit="Your form is successfully submitted. Thank you!" method="post">
+  Full Name: <input type="text" name="fname">
+  Email Address: <input type="text" name="fname">
+ Address: <input type="text" name="fname">
+  Phone Number: <input type="text" name="fname">
+  Enter your Feedback here...: <input type="text" name="fname">
+  <input type="send" value="Submit">
+</form>
